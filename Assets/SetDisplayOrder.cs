ad126863cc232c75b6beb56b8a1814cb055d7e6e
Francis <frytkihog@gmail.com>
2022-11-15 03:22:30 +0100

    new assets

--- Assets/SetDisplayOrder.cs
+++ Assets/SetDisplayOrder.cs
@@ -1,20 +1,35 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SetDisplayOrder : MonoBehaviour
 {
+
+    TilemapRenderer TilemapRenderer;
     SpriteRenderer SpriteRenderer;
 
     [SerializeField] int OffsetY;
+    [SerializeField] bool isTileSet = false;
 
     private void Awake()
     {
-        SpriteRenderer = GetComponent<SpriteRenderer>();
+        if(isTileSet)
+            TilemapRenderer = GetComponent<TilemapRenderer>();
+        else 
+            SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (isTileSet)
+            TilemapRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100f) - OffsetY;
+        else
+            SpriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100f) - OffsetY;
     }
 
     private void FixedUpdate()
     {
         if (!transform.hasChanged) return;
 
-        SpriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100f) - OffsetY;
+        if (isTileSet)
+            TilemapRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100f) - OffsetY;
+        else
+            SpriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 100f) - OffsetY;
     }
 }

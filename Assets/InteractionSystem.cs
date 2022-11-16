5232ac7ef401aca8b5d06bd6aa34372828866fbc
Francis <frytkihog@gmail.com>
2022-11-16 02:29:29 +0100

    Interaction System mostly working with rotations

--- Assets/InteractionSystem.cs
+++ Assets/InteractionSystem.cs
@@ -13,6 +13,9 @@ public class InteractionSystem : MonoBehaviour
     [SerializeField] GameObject CharacterName; TextMeshProUGUI CharacterNameText => CharacterName.GetComponentInChildren<TextMeshProUGUI>();
     [SerializeField] GameObject PlayerName;
 
+    [SerializeField] RectTransform RectCharacterImage;
+    [SerializeField] RectTransform RectPlayerImage;
+
     string TextToDisplay { get; set; } = "";
 
     [Range(0f, 0.1f)] public float timeToTypeCharacter = .05f;
@@ -22,6 +25,7 @@ public class InteractionSystem : MonoBehaviour
     private void Awake()
     {
         Singleton = this;
+
         if (InteractionUI.activeSelf) InteractionUI.SetActive(false);
     }
 
@@ -29,7 +33,11 @@ public class InteractionSystem : MonoBehaviour
     {
         if (!interaction) { Debug.LogError("interaction is null!"); return; }
 
+        Time.timeScale = 0f;
+
         InteractionUI.SetActive(true);
+
+        StopAllCoroutines();
 
         StartCoroutine(DisplayText(interaction.texts[0], interaction.isTextPlayer[0]));
     }
@@ -41,6 +49,20 @@ public class InteractionSystem : MonoBehaviour
 
     IEnumerator DisplayText(string text, bool isTextPlayer)
     {
+        CharacterName.SetActive(!isTextPlayer);
+        PlayerName.SetActive(isTextPlayer);
+
+        if (isTextPlayer)
+        {
+            StartCoroutine(StartRotating(RectPlayerImage, 3f));
+            StartCoroutine(StartRotating(RectCharacterImage));
+        }
+        else
+        {
+            StartCoroutine(StartRotating(RectCharacterImage, 3f));
+            StartCoroutine(StartRotating(RectPlayerImage));
+        }
+
         TextToDisplay = "";
 
         string textToDisplay = "";
@@ -53,7 +75,38 @@ public class InteractionSystem : MonoBehaviour
 
             if (i != text.Length - 1) TextToDisplay += "_";
 
-            yield return new WaitForSeconds(timeToTypeCharacter);
+            yield return new WaitForSecondsRealtime(timeToTypeCharacter);
         }   
     }
+    
+    IEnumerator StartRotating(RectTransform objectToRotate, float RotationSpeed = 2, float MaxRotation = 5) // in anglesPerSecond
+    {
+        if(MaxRotation == 0 || !objectToRotate || RotationSpeed == 0) yield break;
+
+        bool isRotatingRight = Random.Range(0, 2) == 0;
+
+        while (true)
+        {
+            if (Mathf.Abs(objectToRotate.eulerAngles.z) < 30f)
+            {
+                if (Mathf.Abs(objectToRotate.eulerAngles.z) > Mathf.Abs(MaxRotation))
+                {
+                    isRotatingRight = !isRotatingRight;
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(360f - objectToRotate.eulerAngles.z) > Mathf.Abs(MaxRotation))
+                {
+                    isRotatingRight = !isRotatingRight;
+                }
+            }
+            
+
+            objectToRotate.eulerAngles = new(0, 0, (objectToRotate.eulerAngles.z + RotationSpeed * Time.fixedUnscaledDeltaTime * (isRotatingRight ? 1 : -1)));
+
+            yield return new WaitForSecondsRealtime(0.02f);
+        }
+    }
+    
 }

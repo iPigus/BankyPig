3b619aaf924762f57d608e0ff51fb1a1e0c8bd26
Francis <frytkihog@gmail.com>
2022-11-23 23:08:37 +0100

    new assets

--- Assets/Scripts/Characters/BlackHatGuyInteractions.cs
+++ Assets/Scripts/Characters/BlackHatGuyInteractions.cs
@@ -13,14 +13,17 @@ public class BlackHatGuyInteractions : InteractionsList
     };
     List<bool> isPlayerText0 = new List<bool>() { false, true, false, true, false, false, true, false };
 
-    List<string> interactionTexts1 = new(){ "Oh you have found this key, nice", "Can you show me it to me a while?", "???", "Bro I wanna open these door...", "...And?", "And I'll let you in if it works", "Fine then..."
-    };
+    List<string> interactionTexts1 = new(){ "Oh you have found this key, nice", "Can you show me it to me a while?", "???", "Bro I wanna open these door...", "...And?", "And I'll let you in if it works", "Fine then..."};
     List<bool> isPlayerText1 = new List<bool>() { false, false, true, false, true, false, true };
+    
+    List<string> interactionTexts2 = new(){ "XDD, nice", "Can you show me it to me a while?", "???", "Bro I wanna open these door...", "...And?", "And I'll let you in if it works", "Fine then..."};
+    List<bool> isPlayerText2 = new List<bool>() { false, false, true, false, true, false, true };
 
     private void Awake()
     {
         AddInteraction(interactionTexts0, isPlayerText0);
         AddInteraction(interactionTexts1, isPlayerText1);
+        AddInteraction(interactionTexts2, isPlayerText2);
 
         InteractionEnded.AddListener(EventCheck);
     }
@@ -33,13 +36,17 @@ public class BlackHatGuyInteractions : InteractionsList
     {
         return Interactions[GetActiveInteractionIndex()];
     }
-
+    bool isEvent0Invoked = false;
     public override int GetActiveInteractionIndex()
     {
         if (PlayerInventory.doesInventoryContainItem(1))
             return 1;
         else
+        {
+            if (isEvent0Invoked) return 2;
+
             return 0;
+        }
     }
 
     public void EventCheck()
@@ -48,7 +55,7 @@ public class BlackHatGuyInteractions : InteractionsList
 
         switch (GetActiveInteractionIndex())
         {
-            case 1:  EventItemSystem.InvokeEvent("DoorOpening0"); break;
+            case 1:  EventItemSystem.InvokeEvent("DoorOpening0"); isEvent0Invoked = true; break;
             default: break;
         }
     }

70e2e8bf54ae9de1d80b2fa49bedcec0f83cc235
Francis <frytkihog@gmail.com>
2022-11-25 15:27:42 +0100

    changed enemydamage working with colliders too

--- Assets/EnemyDamage.cs
+++ Assets/EnemyDamage.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
     [Range(1, 100)] public int Damage = 1;
+    [SerializeField] bool isTrigger = true;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void EnterZone(GameObject collision, bool IsTrigger)
     {
-        if (!collision.CompareTag("Player")) return;
+        if (!collision.CompareTag("Player") || IsTrigger != isTrigger) return;
 
         PlayerHealth.Damage(Damage);
     }
+    void ExitZone(GameObject collision, bool IsTrigger)
+    {
+        if (!collision.CompareTag("Player") || IsTrigger != isTrigger) return;
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnterZone(collision.gameObject, true);
+    }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Player")) return;
-    
-    
+        ExitZone(collision.gameObject, true);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        EnterZone(collision.gameObject, false);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        ExitZone(collision.gameObject, false);
     }
 }

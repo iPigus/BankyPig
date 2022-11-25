784ddeffbee02eb8bb8c4f80f32842bbc5d4ea7b
Francis <frytkihog@gmail.com>
2022-11-25 15:32:45 +0100

    added cooldown for enemy attacks

--- Assets/EnemyDamage.cs
+++ Assets/EnemyDamage.cs
@@ -7,11 +7,32 @@ public class EnemyDamage : MonoBehaviour
 {
     [Range(1, 100)] public int Damage = 1;
     [SerializeField] bool isTrigger = true;
+    [Range(0f, 1f)][SerializeField] float Cooldown = .2f;
+
+    bool _isCooldown;
+    bool isCooldown
+    {
+        get => _isCooldown;
+        set
+        {
+            if (!value || isCooldown) return;
+
+            StartCoroutine(Cooldowning());
+        }
+    }
+
+    IEnumerator Cooldowning()
+    {
+        yield return new WaitForSeconds(Cooldown);
+
+        _isCooldown = false;
+    }
 
     void EnterZone(GameObject collision, bool IsTrigger)
     {
-        if (!collision.CompareTag("Player") || IsTrigger != isTrigger) return;
+        if (!collision.CompareTag("Player") || IsTrigger != isTrigger || isCooldown) return;
 
+        isCooldown = true;
         PlayerHealth.Damage(Damage);
     }
     void ExitZone(GameObject collision, bool IsTrigger)
